c3079d072d2011563b0012f3c77563ab2d339e52
Carlos Rocha <crocha09.09@gmail.com>
2022-11-04 20:36:31 -0600

    fix bug home controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,13 @@
 class HomeController < ApplicationController
   def index
+    ad_ids = []
     if account_signed_in?
-      ad_ids = []
       tags = current_account.seens
       tags.each do |single|
         ad_ids.append(single.announcement.id)
       end
-
+    end
+    if ad_ids.count > 0
       @anuncios = Announcement.where.not('id IN (?)', ad_ids).order(created_at: :desc)
     else
       @anuncios = Announcement.order(created_at: :desc)
@@ -14,6 +15,5 @@ class HomeController < ApplicationController
   end
 
   def about
-    
   end
 end
